7cb433d5490e947538fabc9621dbee2db38a3ea4
Dennis <dennis.vashchuk@gmail.com>
2020-06-08 22:00:03 -0700

    Add unique-item-in-arr2.cpp

---
+++ src/unique-item-in-arr2.cpp
@@ -0,0 +1,20 @@
+// 23) Find the number which is not repeated in Array of integers, others are present for two times: https://www.geeksforgeeks.org/top-25-interview-questions/?ref=rp
+
+#include "util.hpp"
+#include <unordered_map>
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    vector<int> arr = input_vector<int>();
+    print(arr);
+
+    int x = 0;
+    for (const auto& i: arr) {
+        x ^= i;
+    }
+    std::cout << x << std::endl;
+}
